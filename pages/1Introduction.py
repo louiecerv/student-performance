4b8dbb54ed6905852e16f261a266bd8c45764759
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-09 01:34:46 +0800

    Save

--- pages/1Introduction.py
+++ pages/1Introduction.py
@@ -11,7 +11,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.datasets import fetch_openml
 
 # Define the Streamlit app
 def app():

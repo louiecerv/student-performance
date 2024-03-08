0a830b33efa74c19942d92b9e7a4d6bf430e6815
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-08 16:55:00 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import streamlit as st
 import altair as alt
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report

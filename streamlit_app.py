2c44cdf4c38820291f718df125713fc15aa89108
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-08 16:29:06 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,48 @@ from sklearn.metrics import classification_report
 
 # Define the Streamlit app
 def app():
-    st.write('Home Page')
+
+    if "reset_app" not in st.session_state:
+        st.session_state.reset_app = False
+
+    text = """Decision Tree, Random Forest and K-Nearest Neighbor on the MNIST Dataset"""
+    st.subheader(text)
+
+    # Use session state to track the current form
+    if "current_form" not in st.session_state:
+        st.session_state["current_form"] = 0
+
+    if "clf" not in st.session_state: 
+        st.session_state["clf"] = []
+
+    if "X_train" not in st.session_state: 
+        st.session_state["X_train"] = []
+
+    if "X_test" not in st.session_state: 
+        st.session_state["X_test"] = []
+    
+    if "y_train" not in st.session_state: 
+        st.session_state["X_train"] = []
+    
+    if "y_test" not in st.session_state: 
+        st.session_state["y_yest"] = []
+
+    if "selected_model" not in st.session_state: 
+        st.session_state["selected_model"] = 0
+
+    text = """Louie F. Cervantes, M. Eng. (Information Engineering) \n\n
+    CCS 229 - Intelligent Systems
+    Computer Science Department
+    College of Information and Communications Technology
+    West Visayas State University"""
+    st.text(text)
+
+    st.image('MNIST.png', caption="Modified National Institute of Standards and Technology", use_column_width=True)
+    text = """MNIST is a large database of handwritten digits that is commonly used for training and
+    testing various image processing systems. The acronym stands for Modified National Institute 
+    of Standards and Technology. MNIST is a popular dataset in the field of machine learning and 
+    can provide a baseline for benchmarking algorithms."""
+    st.write(text)
     
 #run the app
 if __name__ == "__main__":

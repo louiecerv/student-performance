8e51e39ede4affbd940bc72522b7311dce17c59a
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-10 17:56:43 +0800

    Save

--- E-Banking_Usage.py
+++ E-Banking_Usage.py
@@ -76,7 +76,7 @@ def app():
         #replace with your dataset
         df = pd.read_csv('e-banking.csv', header=0)
         # exclude the second target column
-        df.drop('Influence', axis=1, inplace=True)
+        df = df.drop('Influence', axis=1, inplace=True)
         st.session_state['df'] = df
         
         # load the data and the labels

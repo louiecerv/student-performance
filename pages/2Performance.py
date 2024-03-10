1ea975e178257ef319bf6bd1086ea65b2794514f
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-10 19:47:50 +0800

    Save

--- pages/2Performance.py
+++ pages/2Performance.py
@@ -5,25 +5,26 @@ import streamlit as st
 import altair as alt
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix
+
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 
 # Define the Streamlit app
 def app():
 
     classifier = ''
-    if st.session_state['selected_model'] == 0:     # decision tree
+    if st.session_state['selected_model'] == 0:     # Logistic Regression
         report = """Achieves good accuracy, but can be prone to 
         overfitting, leading to lower performance on unseen data.
         Simple and interpretable, allowing visualization of decision rules.
         Susceptible to changes in the training data, potentially 
         leading to high variance in predictions."""
-        classifier = 'Decision Tree'
-    elif st.session_state['selected_model'] == 1:   # Extreme Random Forest
+        classifier = 'Logistic Regression'
+    elif st.session_state['selected_model'] == 1:   # SVR
         report = """Performance: Can achieve similar or slightly better 
         accuracy compared to a random forest, but results can vary 
         depending on hyperparameter tuning. Introduces additional randomness 
@@ -31,8 +32,17 @@ def app():
         further improve generalization and reduce overfitting by increasing 
         the diversity of trees in the ensemble. Requires careful 
         hyperparameter tuning to achieve optimal performance."""
-        classifier = "Extreme Random Forest"
-    else:
+        classifier = "SVR Regressor"
+    elif st.session_state['selected_model'] == 2:   # Decision Tree
+        report = """Performance: Can achieve similar or slightly better 
+        accuracy compared to a random forest, but results can vary 
+        depending on hyperparameter tuning. Introduces additional randomness 
+        during tree building by randomly selecting features at each split.  Aims to 
+        further improve generalization and reduce overfitting by increasing 
+        the diversity of trees in the ensemble. Requires careful 
+        hyperparameter tuning to achieve optimal performance."""
+        classifier = "Decision Tree"
+    else:   #Gradient Boosting
         report = """Gradient boosting classifiers perform well on the MNIST 
         handwritten digit dataset, typically achieving accuracy in the high 90s 
         (often exceeding 98%). Gradient boosting excels at handling complex, 
@@ -49,33 +59,38 @@ def app():
     y_train= st.session_state['y_train']
     y_test = st.session_state['y_test']
 
+    clf = LogisticRegression()
+    clf = SVR()
+    DecisionTreeRegressor()
+    GradientBoostingRegressor()
+
     st.sidebar.subheader('Select the classifier')
 
     # Create the selection of classifier
-    clf = DecisionTreeClassifier()
+    clf = LogisticRegression()
     st.session_state['selected_model'] = 0
-    options = ['Decision Tree', 'Extreme Random Forest Classifier', 'Gradient Boosting Classifier']
+    options = ['Logistic Regression', 'SVR Regressor', 'Decision Tree', 'Gradient Boosting']
     selected_option = st.sidebar.selectbox('Select the classifier', options)
-    if selected_option=='Extreme Random Forest Classifier':        
-        clf = ExtraTreesClassifier(n_estimators=100, max_depth=4, random_state=0)
+    if selected_option=='SVR Regressor':        
+        clf = SVR()
         st.session_state['selected_model'] = 1
-    elif selected_option == 'Gradient Boosting Classifier':
-        clf = GradientBoostingClassifier()
+    elif selected_option == 'Decision Tree':
+        clf = DecisionTreeRegressor()
         st.session_state['selected_model'] = 2
-
-    # save the clf to the session variable
-    st.session_state['clf'] = clf
+    elif selected_option == 'Gradient Boosting':
+        clf = GradientBoostingRegressor()
+        st.session_state['selected_model'] = 3
 
     clf.fit(X_train, y_train)
     y_test_pred = clf.predict(X_test)
 
-    st.subheader('Confusion Matrix')
-    st.write('Confusion Matrix')
-    cm = confusion_matrix(y_test, y_test_pred)
-    st.text(cm)
+    st.subheader('R-squared')
+    r2 = r2_score(y_test, y_test_pred)
+    st.write(f"R-squared: {r2:.4f}")
 
-    st.subheader('Performance Metrics')
-    st.text(classification_report(y_test, y_test_pred))
+    st.subheader('Mean Squared Error (MSE)')
+    mse = mean_squared_error(y_test, y_test_pred)
+    st.write(f"MSE: {mse:.4f}")
     
     st.write(classifier)
     st.write(report)

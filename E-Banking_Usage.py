d40895978127c4b934a27e64c7a4e8eb35eeaf32
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-11 08:12:07 +0800

    Save

--- E-Banking_Usage.py
+++ E-Banking_Usage.py
@@ -18,8 +18,7 @@ import time
 def app():
 
 
-    text = """E-banking Usage level and Influence on Spending Habits Among 
-        College of Business and Management Students"""
+    text = """E-banking Usage level and Influence on Spending Habits Among College of Business and Management Students"""
     st.subheader(text)
 
     # Use session state to track the current form

e105bdb71c4ee64edff01058408d4e21ad2ff5f0
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-11 11:03:00 +0800

    Save

--- E-Banking_Usage.py
+++ E-Banking_Usage.py
@@ -53,7 +53,7 @@ def app():
     st.image('e-banking.jpg', caption="The E-banking Usage")
     text = """Dataset for E-Banking Usage and Spending Habits of CBM Students at WVSU is used 
     with permission from an undergraduate thesis paper:
-    \Baldevarona, A. M. G., Banggud, R. B., Gronifillo, G. C., Honrado, S. R. B., & Jamelarin, 
+    \nBaldevarona, A. M. G., Banggud, R. B., Gronifillo, G. C., Honrado, S. R. B., & Jamelarin, 
     R. J. E. L. (2024). E-banking usage level and influence on spending habits among 
     College of Business and Management students. 
 
